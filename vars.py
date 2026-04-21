c7798697ef1c530cbf22da308809103f7eb96f2b
opljkl802-bit <opljkl802@gmail.com>
2026-04-21 02:17:01 -0700

    Update vars.py

--- vars.py
+++ vars.py
@@ -2,9 +2,9 @@
 
 from os import environ
 
-API_ID = int(environ.get("API_ID","30296254", ""))
-API_HASH = environ.get("API_HASH","c2b5306f4ccd2d795405a026c10b4c62", "")
-BOT_TOKEN = environ.get("BOT_TOKEN","8697727988:AAHFPX-fMxt61jqOyFlJEbLJO7EwR1TEP4M", "")
+API_ID = int(environ.get("API_ID", "30296254"))
+API_HASH = environ.get("API_HASH", "c2b5306f4ccd2d795405a026c10b4c62")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8697727988:AAHFPX-fMxt61jqOyFlJEbLJO7EwR1TEP4M")
 
 # Force Subscribe Configuration
 FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "gkfjldlkfvoifg")  # Channel username without @, 
