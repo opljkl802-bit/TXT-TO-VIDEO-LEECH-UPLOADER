dcc5f13862e64a86bb5e93cd2c644b2844b28d00
opljkl802-bit <opljkl802@gmail.com>
2026-05-05 02:26:12 -0700

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,25 +1,5 @@
+import os
 
-
-from os import environ
-
-API_ID    = int(environ.get("API_ID", ""))
-API_HASH  = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "") 
-
-# Force Subscribe Configuration
-FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "gkfjldlkfvoifg")  # Channel username without @, 
-FORCE_SUB_CHANNEL_LINK = environ.get("FORCE_SUB_CHANNEL_LINK", "https://t.me/gkfjldlkfvoifg")  # Channel link
-
-# Admin Configuration
-ADMINS = list(map(int, environ.get("6320092636", "").split()))
-
-# Optional: Bot Owner ID
-OWNER_ID = environ.get("7660916897", "")
-
-# Database URL (if you want to add database support later)
-DATABASE_URL = environ.get("DATABASE_URL", "")
-
-
-
-
-
+API_ID    = os.environ.get("API_ID", "30296254")
+API_HASH  = os.environ.get("API_HASH", "c2b5306f4ccd2d795405a026c10b4c62")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8614201321:AAFYogtipBDjlWBhTQGXJTcYuX3co4S0t4c") 
